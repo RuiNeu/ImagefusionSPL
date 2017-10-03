a50afec4fc8ecb9892753f89dec0699807f5bba4
Rui <31206742+RuiNeu@users.noreply.github.com>
2017-10-03 13:05:13 +0300

    Update Fusion.m

--- matlab/Proposed/Fusion.m
+++ matlab/Proposed/Fusion.m
@@ -12,7 +12,6 @@ fast_sthresh = @(x,th) sign(x).*max(abs(x) - th,0);
 T = inv(lambda*eye(m)+ mu*(Omega')*Omega);
 
 while (norm(Z-Omega*X,'fro') >= .001)
-    i
     X = T *(mu*Y + mu*Omega'*(Z-Theta));
     Z = fast_sthresh(Omega*X+Theta,1/mu);
     Theta = Theta - (Z-Omega*X);
