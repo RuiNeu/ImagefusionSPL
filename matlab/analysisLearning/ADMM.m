d20355f551da1363be0bdbdce0838f8ee51cc45f
Rui <31206742+RuiNeu@users.noreply.github.com>
2017-10-03 15:18:12 +0300

    Update ADMM.m

--- matlab/analysisLearning/ADMM.m
+++ matlab/analysisLearning/ADMM.m
@@ -1,15 +1,14 @@
-function [Xout] = ADMM(Y,X,Omega,lambda,iter)
+function [Xout] = ADMM(Y,X,Omega,gamma,iter)
 
-gamma = lambda; 
 D = zeros(size(Omega,1),size(X,2));   
 V = Omega*X; % Parameter Splitting
 fast_sthresh = @(x,th) sign(x).*max(abs(x) - th,0);
 t = 1;
 fast_sthresh = @(x,th) sign(x).*max(abs(x) - th,0);
-while (t <= 5) || (norm(V-Omega*X,'fro') >= .001) && (t <= iter)
-    X = (lambda*Y + gamma*Omega'*(V-D))/(lambda+gamma);
+while (norm(V-Omega*X,'fro') >= .001) && (t <= iter)
+    X = (lambda*Y + gamma*Omega'*(V-D))/(lambda+gamma);  % admm code
     Z = fast_sthresh(Omega*X+D,1/gamma);
-    D = D - (V-Omega*X); % Updating the Lagrange parameter of ADMM
+    D = D - (V-Omega*X); 
     t = t + 1;
 end
 Xout = X;
