faf0bf1a78019dd7f4e3c025e489b48e17b853e2
Rui <31206742+RuiNeu@users.noreply.github.com>
2017-10-03 13:08:56 +0300

    Add files via upload

--- matlab/SR-KSVD/Evaluation/Hab.m
+++ matlab/SR-KSVD/Evaluation/Hab.m
@@ -6,17 +6,18 @@ function HabR=Hab(grey_matrixA,grey_matrixB,grey_level)
 
 [row,column]=size(grey_matrixA);
 counter = zeros(256,256);
-%统计直方图
+
 grey_matrixA=grey_matrixA+1;
 grey_matrixB=grey_matrixB+1;
+
 for i=1:row
     for j=1:column
         indexx = grey_matrixA(i,j);
         indexy = grey_matrixB(i,j);
-        counter(indexx,indexy) = counter(indexx,indexy)+1;%联合直方图
+        counter(indexx,indexy) = counter(indexx,indexy)+1;SSSSSSS
     end
 end
-%计算联合信息熵
+
 total= sum(counter(:));
 index = find(counter~=0);
 p = counter/total;
--- matlab/SR-KSVD/Evaluation/entropy_fusion.m
+++ matlab/SR-KSVD/Evaluation/entropy_fusion.m
@@ -5,8 +5,8 @@ function entropyR=entropy_fusion(grey_matrix,grey_level)
 % Website: http://quxiaobo.go.8866.org
 [row,column]=size(grey_matrix);
 total=row*column;
-% grey_level=256 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
 counter=zeros(1,grey_level);
 grey_matrix=grey_matrix+1;
 for i=1:row
