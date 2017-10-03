6c6695aaafec99824807349c4c4611173e67e866
Rui <31206742+RuiNeu@users.noreply.github.com>
2017-10-03 15:31:32 +0300

    Update OmegaUpdate.m

--- matlab/analysisLearning/OmegaUpdate.m
+++ matlab/analysisLearning/OmegaUpdate.m
@@ -27,3 +27,11 @@ O_out = svdDec(O')';
 
 function obj = f(O,X)
 obj = sum(sum(abs(O*X)));
+
+function X_out = NorSvdDec(X)
+X_out = X;
+[h, m] = size(X);
+t = sqrt(h/m);
+for ind = 1:m 
+    X_out(:,ind) = t * X_out(:,ind) / sqrt(X_out(:,ind)'*X_out(:,ind));
+end
