bcd0e510d7394e814d32c0e2fa50baa6eb46478a
Rui <31206742+RuiNeu@users.noreply.github.com>
2017-10-03 12:53:12 +0300

    Add files via upload

--- matlab/SF-DCT/Evaluation/Evaluation.m
+++ matlab/SF-DCT/Evaluation/Evaluation.m
@@ -6,6 +6,6 @@ function Result=Evaluation(grey_matrixA,grey_matrixB,fusion_matrix,grey_level)
 
 Result=zeros(1,2);
 
-Result(1,1)=mutural_information(grey_matrixA,grey_matrixB,fusion_matrix,grey_level);
-Result(1,2)=edge_association(grey_matrixA,grey_matrixB,fusion_matrix);
-disp('|| MI || QAB/F ||')
+Result(1,1) = mutural_information(grey_matrixA,grey_matrixB,fusion_matrix,grey_level);
+Result(1,2) = edge_association(grey_matrixA,grey_matrixB,fusion_matrix);
+disp(' Q_MI && Q_ABF ')
